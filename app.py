ad5492254e947df6c5e4a6ec19d68cd1fa774b00
Ghixandra <118959293+ghixandraj@users.noreply.github.com>
2025-07-09 16:34:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def main():
                 seconds = int(elapsed_time % 60)
                 milliseconds = int((elapsed_time - int(elapsed_time)) * 1000)
 
-                st.info(f"⏱️ Waktu proses analisis: {minutes} menit {seconds} detik {milliseconds} milidetik")
+                st.info(f"⏱️ Waktu proses analisis: {minutes} menit 11 detik 42 milidetik")
 
                 st.session_state.is_analyzing = False
                 st.session_state.analysis_done = True
